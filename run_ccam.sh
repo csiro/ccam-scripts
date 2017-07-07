eba5513847017a40daee0c84515f7905d3a8e199
Thatcher, Marcus (O&A, Aspendale) <Marcus.Thatcher@csiro.au>
2017-07-07 11:56:57 +1000

    Update for run_ccam.sh for new users

--- run_ccam.sh
+++ run_ccam.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=ccam
-#SBATCH --mem=40gb
-#SBATCH --nodes=5
-#SBATCH --ntasks-per-node=20
-#SBATCH --time=02:00:00
-#SBATCH --qos express
+#SBATCH --mem=96gb
+#SBATCH --ntasks-per-node=50
+#SBATCH --cores-per-socket=10
+#SBATCH --time=24:00:00
 
 ###############################################################
 # PEARCEY MODULES
 
-module unload openmpi
-module load openmpi/1.8.8-mellanox
-#module load mpt
+module load mpt
 module load netcdf/4.3.3.1
 module load python
 
-
-
-# RAIJIN MODULES
-#module load python/2.7.5
-#module load python/2.7.5-matplotlib
-#module load hdf5
-#module load netcdf
-
-#NETCDF4_DIR=$NETCDF_ROOT pip install --user --build $TMPDIR/pip_build netcdf4
-
 ###############################################################
 # This is the CCAM run script
-
-# set -xv
 
 # Example grid sizes and processor numbers
 # (NOTE: CCAM will automatically adjust the number of processors. Hence we
@@ -46,12 +31,15 @@ module load python
 ###############################################################
 # Specify parameters
 
+insdir=$HOME/ccaminstall                     # install directory
+hdir=$insdir/scripts/run_ccam                # script directory
+
 nproc=$SLURM_NTASKS                          # number of processors
 
-midlon=135.5                                 # central longitude of domain
-midlat=-34.5                                 # central latitude of domain
+midlon=0.                                    # central longitude of domain
+midlat=0.                                    # central latitude of domain
 gridres=-999.                                # required resolution (km) of domain (-999.=automatic)
-gridsize=48                                  # cubic grid size 
+gridsize=96                                  # cubic grid size 
 
 name=ccam_${gridres}km                       # run name
 
@@ -60,11 +48,10 @@ if [[ $gridres = "-999." ]]; then
   name=`echo $name | sed "s/$gridres/$gridtxt"/g`
 fi
 
-mlev=27                                      # number of model levels (27, 35, 54, 72, 108 or 144)
-iys=2005                                     # start year
-ims=01                                       # start month
-iye=2005                                     # end year
-ime=01                                       # end month
+iys=2000                                     # start year
+ims=1                                        # start month
+iye=2000                                     # end year
+ime=1                                        # end month
 leap=1                                       # Use leap days (0=off, 1=on)
 ncountmax=12                                 # Number of months before resubmit
 
@@ -77,39 +64,43 @@ reqres=-999.                                 # required output resolution (degre
 plevs="1000, 850, 700, 500, 300"             # output pressure levels (hPa)
 
 dmode=0                                      # downscaling (0=spectral(GCM), 1=SST-only, 2=spectral(CCAM) )
+cmip=cmip5                                   # CMIP scenario (CMIP3 or CMIP5)
+rcp=RCP45                                    # RCP scenario (historic, RCP45 or RCP85)
 nstrength=0                                  # nudging strength (0=normal, 1=strong)
+mlev=35                                      # number of model levels (27, 35, 54, 72, 108 or 144)
 sib=1                                        # land surface (1=CABLE, 2=MODIS, 3=CABLE+SLI)
 aero=1                                       # aerosols (0=off, 1=prognostic)
 conv=1                                       # convection (0=2014, 1=2015a, 2=2015b, 3=2017)
 cloud=2                                      # cloud microphysics (0=liq+ice, 1=liq+ice+rain, 2=liq+ice+rain+snow+graupel)
 bmix=0                                       # boundary layer (0=Ri, 1=TKE-eps)
-river=1                                      # river (0=off, 1=on)
+river=0                                      # river (0=off, 1=on)
 mlo=0                                        # ocean (0=Interpolated SSTs, 1=Dynamical ocean)
 casa=0                                       # CASA-CNP carbon cycle with prognostic LAI (0=off, 1=CASA-CNP, 2=CASA-CN+POP)
 
-ncout=1                                      # standard output format (0=none, 1=CCAM, 2=CORDEX, 3=CTM)
+ncout=2                                      # standard output format (0=none, 1=CCAM, 2=CORDEX, 3=CTM)
 nctar=1                                      # TAR output files in OUTPUT directory (0=off, 1=on)
 ncsurf=0                                     # High-freq output (0=none, 1=lat/lon, 2=raw)
-ktc_surf=5                                   # High-freq file output period (mins)
+ktc_surf=10                                  # High-freq file output period (mins)
+
+###############################################################
+# Host atmosphere for dmode=0 or dmode=2
 
 bcdom=ccam_eraint_                           # host file prefix for dmode=0 or dmode=2
+bcdir=$insdir/erai                           # host atmospheric data (dmode=0 or dmode=2)
+
+###############################################################
+# Sea Surface Temperature for dmode=1
+
+sstfile=ACCESS1-0_RCP45_bcvc_osc_ots_santop96_18_0.0_0.0_1.0.nc # sst file for dmode=1
+sstinit=$bcdir/$bcdom$iys$ims.nc                                # initial conditions file for dmode=1
+sstdir=$insdir/gcmsst                                           # SST data (dmode=1)
 
 ###############################################################
 # Specify directories
 
-cmip=cmip5                            # CMIP scenario
-rcp=RCP45                             # RCP scenario
-insdir=/datastore/tha051/ccaminstall  # install directory
 excdir=$insdir/scripts/run_ccam       # python code directory
-hdir=$FLUSHDIR/ccam/wangery           # script directory
-bcdir=$insdir/erai                    # host atmospheric data (dmode=0 or dmode=2)
-sstdir=$insdir/gcmsst                 # SST data (dmode=1)
 stdat=$insdir/ccamdata                # eigen and radiation datafiles
 vegca=$hdir/vegdata                   # topographic datasets
-
-sstfile=ACCESS1-0_RCP45_bcvc_osc_ots_santop96_18_0.0_0.0_1.0.nc # sst file for dmode=1
-sstinit=$bcdir/$bcdom$iys$ims.nc                                # initial conditions file for dmode=1
-
 
 ###############################################################
 # Specify executables
@@ -118,10 +109,9 @@ terread=$insdir/src/bin/terread
 igbpveg=$insdir/src/bin/igbpveg
 ocnbath=$insdir/src/bin/ocnbath
 casafield=$insdir/src/bin/casafield
-
 aeroemiss=$insdir/src/bin/aeroemiss
-model=$HOME/bin/globpea.1707f_1.8.8
-pcc2hist=$HOME/bin/pcc2hist_1.8.8
+model=$insdir/src/bin/globpea
+pcc2hist=$insdir/src/bin/pcc2hist
 
 ###############################################################
 
