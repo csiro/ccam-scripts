b5f17309ebe1a36c16137000256b61609703aab3
Thatcher, Marcus (O&A, Aspendale) <Marcus.Thatcher@csiro.au>
2019-09-12 10:06:44 +1000

    Update to ocean configuration ub run_ccam.py

--- run_ccam.py
+++ run_ccam.py
@@ -1318,8 +1318,8 @@ def input_template_6():
      ateb_zoroof=0.05 ateb_zocanyon=0.05
     &end
     &mlonml
-     mlodiff=1 mlomfix=2 otaumode=1 mlojacobi=2
-     usetide=0 mlosigma=2
+     mlodiff=1 mlomfix=2 otaumode=1 mlojacobi=7
+     usetide=0 mlosigma=5
      rivermd=1
     &end
     &tin &end
